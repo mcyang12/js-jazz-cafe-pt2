ae399d4871ecaab58440a1d420d52756706ebba2
Mai <myang012@gmail.com>
2024-03-11 15:42:28 -0500

    started

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,14 @@
 // Functional Programming Tasks:
 
 // Task 1: Implement a function that calculates total earnings
-
+function calculateEarnings(earnings){
+    let total = 0;
+    for (const earning of earnings){
+        total += earning.amount;
+    }
+}
     // Calculate the total earnings by summing up ticket prices
+    return total;
 
 // Examples to test in console:
 const ticketPrices = [20, 25, 30];
@@ -10,7 +16,14 @@ const totalEarnings = calculateEarnings(ticketPrices);
 console.log("Total Earnings:", totalEarnings);
 
 // Task 2: Implement a function to filter out free events from a list of events
-
+function filterFreeEvents(events){
+    let paidEvents = [];
+    for (const event of events){
+        if (event.paid > 0, paidEvents.push(events)){
+    }
+    return paidEvents;
+    }
+}
     // Create an array to store paid events
 
     // Iterate through each event
@@ -37,7 +50,12 @@ console.log("Paid Events:", paidEvents);
 // Object-Oriented Programming Tasks:
 
 // Task 3: Define a class for Jazz Musicians
-
+class JazzMusician{
+    constructor(name, instrument){
+        this.name = name;
+        this.instrument = instrument;
+    }
+}
 
 // Examples to test in console:
 const musician = new JazzMusician("Louis Armstrong", "Trumpet");
@@ -45,7 +63,11 @@ const musician = new JazzMusician("Louis Armstrong", "Trumpet");
 console.log("Jazz Musician:", musician);
 
 // Task 4: Extend the JazzMusician class for Vocalists
-
+class vocalist extends JazzMusician{
+    constructor(name, style){
+        super(name, style);
+    }
+}
 
 // Examples to test in console:
 const vocalist = new Vocalist("Ella Fitzgerald", "Scat");
@@ -57,7 +79,12 @@ console.log("Jazz Vocalist:", vocalist);
 // Classes and Constructors Tasks:
 
 // Task 5: Create a custom constructor function for Jazz Bands
-
+function JazzBand(name, genre){
+    let band = {};
+    band.name = name;
+    band.genre = genre;
+    return band;
+}
     // Create a new object with the specified properties
 
     // Return the newly created band object
@@ -70,7 +97,12 @@ console.log("Jazz Band:", band);
 
 // Task 6 : Implement a class with default parameters for Jazz Instruments.
 // The default TYPE should be Saxophone, and the default PLAYER should be John Coltrane.
-
+class JazzInstrument{
+    constructor(name = "John Coltrane", instrument = "saxophone"){
+        this.player = name;
+        this.type = instrument;
+    }
+}
 
 // Examples to test in console:
 const saxophonist = new JazzInstrument();
